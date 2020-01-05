47ce31639992a9f9148314555fae9acce9ac3351
priyankakDec19 <priyanka18031995@gmail.com>
2020-01-05 14:46:34 +0530

    Adding Question1

---
+++ src/priyankaDec19/Assignment1/Assignment1_1.java
@@ -0,0 +1,19 @@
+package priyankaDec19.Assignment1;
+
+class Assignment1_1
+{
+	void Sum(int x, int y)
+	{
+		int sum= x+y;
+		System.out.println("Total is " + sum);
+	}
+	 public static void main(String args[])
+	 {
+		 Assignment1_1 as1= new Assignment1_1();
+		 as1.Sum(30,40);
+	}
+	
+	
+	
+	
+}
