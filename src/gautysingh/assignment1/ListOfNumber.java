315465d38a763f628b7c991bb7008d698de44ac5
Ghanshyam <ggautamcse@gmail.com>
2020-01-05 14:46:17 +0530

    Full Assignment

---
+++ src/gautysingh/assignment1/ListOfNumber.java
@@ -0,0 +1,15 @@
+package gautysingh.assignment1;
+
+public class ListOfNumber 
+{
+    public static void main(String[] args) 
+    {
+      
+        for(int i = 5;i<=10;i++)
+        {
+          System.out.println(i);
+      
+        }
+    }    
+   
+}
