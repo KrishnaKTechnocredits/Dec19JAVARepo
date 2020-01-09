b07a97641b5fdc18c04818031dd6c816041a7afb
Vikas <bidvevikas@gmail.com>
2020-01-10 02:29:57 +0530

    adding solution of assignment1 questions

--- src/bidvevikas/Assignment1/IntegersBetween.java
+++ src/bidvevikas/Assignment1/IntegersBetween.java
@@ -1,14 +1,18 @@
-public class IntegersBetween {
+
+ //5)Write a program to list all the integers between two integers start and end(including start
+and end)
+class IntegersBetween1 {
 	void listIntegers(int start, int end)
 	{
 		for (int i=start; i<=end; i++)
 			{
 				System.out.println(i);
 			}
-	}
 	
-	public static void main(String args[])
+	}
+		public static void main(String args[])
 			{
-				new IntegersBetween().listIntegers(5,10);
+				 IntegersBetween1 sum=new IntegersBetween1();
+				sum.listIntegers(5,10);
 			}
 }
