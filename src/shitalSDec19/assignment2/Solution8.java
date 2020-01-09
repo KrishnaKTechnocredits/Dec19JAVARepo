61dd09f75e3e8195f60e9d0c7b15851f6045c06c
ShitalSDec19 <shitalseware@gmail.com>
2020-01-09 23:53:09 +0530

    Adding all solutions of assignment2 except 1 and 11

---
+++ src/shitalSDec19/assignment2/Solution8.java
@@ -0,0 +1,32 @@
+/*Find first differ index from given arrays 
+ 
+i/p :- arr1: {10,2,9,14,3} arr2: {10,2,18,14,3} Values are not matching at index -> 2 */
+
+package shitalSDec19.assignment2;
+
+public class Solution8 {
+	void differIndex(int [] arr1,int [] arr2)
+	{  
+		if (arr1.length==arr2.length)
+			{
+				for (int i=0; i<arr1.length;i++)
+				{
+					if (arr1[i]!=arr2[i])
+						{
+						System.out.println("Values are not matching at index " + i);
+						break;
+						}
+				}
+			}
+		else
+			System.out.println("array length is mismatch");
+	}
+	public static void main(String args[])
+	{
+		int [] arr1 = {10,2,9,14,3};
+		int [] arr2 = {10,2,18,14,3};
+		Solution8 solution8 = new Solution8();
+		solution8.differIndex(arr1, arr2);
+	}
+
+}
---
+++ src/shitalSDec19/assignment2/Solution9.java
@@ -0,0 +1,33 @@
+/* Find all differ index from given arrays 
+ 
+i/p :-arr1: {10,2,9,14,3}   arr2: {10,2,18,14,13} 
+ 
+o/p :- Values are not matching at index -> 2  Values are not matching at index -> 4  */
+
+	package shitalSDec19.assignment2;
+
+	public class Solution9 {
+		void differIndex(int [] arr1,int [] arr2)
+		{  
+			if (arr1.length==arr2.length)
+				{
+					for (int i=0; i<arr1.length;i++)
+					{
+						if (arr1[i]!=arr2[i])
+							System.out.println("Values are not matching at index " + i);
+					}
+				}
+			else
+				System.out.println("array length is mismatch");
+		}
+		public static void main(String args[])
+		{
+			int [] arr1 = {10,2,9,14,3};
+			int [] arr2 = {10,2,18,14,13};
+			Solution9 solution9 = new Solution9();
+			solution9.differIndex(arr1, arr2);
+		}
+
+	}
+
+
