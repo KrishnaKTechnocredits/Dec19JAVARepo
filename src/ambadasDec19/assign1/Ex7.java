0c411c891409957a1353e4e6c26e63d176292d78
Ambadas Deshpande <ambadasdeshpande016@gmail.com>
2020-01-09 21:06:34 +0530

    Three files get added

---
+++ src/ambadasDec19/assign1/Ex7.java
@@ -0,0 +1,14 @@
+public class Ex7{
+	public void  math()
+	{
+		
+	}
+	public static void main(String[] args) 
+	{
+		Ex7 x7=new Ex7();
+		x7.math();
+		int n = 2;
+		int m =5;
+		System.out.println(Math.abs(n-m));
+	}
+}
