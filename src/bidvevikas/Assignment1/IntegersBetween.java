284afce8c56896ab8f256355b61cb6292b3960a4
Pushparaj Dhole <Pushparaj.Dhole23@gmail.com>
2020-01-09 15:45:25 +0100

    created new branch and added one program for intergers between the range.

--- src/bidvevikas/Assignment1/IntegersBetween.java
+++ src/bidvevikas/Assignment1/IntegersBetween.java
@@ -1,22 +1,3 @@
-
-class A {
-	public void hello()
-	{
-		System.out.println("hello");
-	
-	}
-	
-}
-
-class B {
-	public void hi()
-	{
-		System.out.println("hi");
-	}
-	
-}
-
-
 public class IntegersBetween {
 	void listIntegers(int start, int end)
 	{
@@ -28,8 +9,6 @@ public class IntegersBetween {
 	
 	public static void main(String args[])
 			{
-				new A().hello();
-				new B().hi();
 				new IntegersBetween().listIntegers(5,10);
 			}
 }
