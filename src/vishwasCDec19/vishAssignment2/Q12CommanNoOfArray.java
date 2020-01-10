40401fd0b3dd5108c9afda8a6be9f6e99d59bbe9
Vishwas <vishwaschavan40@gmail.com>
2020-01-10 18:44:06 +0530

    vishAssigmment2 added successfully

---
+++ src/vishwasCDec19/vishAssignment2/Q12CommanNoOfArray.java
@@ -0,0 +1,34 @@
+package vishwasCDec19.vishAssignment2;
+/*
+ * WAP to print common number from two different array
+ */
+
+public class Q12CommanNoOfArray {
+
+	static void printIntersection(int arr1[], int arr2[], int m, int n) 
+	    { 
+	      int i = 0, j = 0; 
+	      while (i < m && j < n) 
+	      { 
+	        if (arr1[i] < arr2[j]) 
+	          i++; 
+	        else if (arr2[j] < arr1[i]) 
+	          j++; 
+	        else 
+	        { 
+	          System.out.print(arr2[j++]+" "); 
+	          i++; 
+	        } 
+	      } 
+	    } 
+	      
+	    public static void main(String args[]) 
+	    { 
+	        int arr1[] = {1, 2, 4, 5, 6}; 
+	        int arr2[] = {2, 3, 5, 7}; 
+	        int m = arr1.length; 
+	        int n = arr2.length; 
+	        printIntersection(arr1, arr2, m, n); 
+	    } 
+	} 
+
