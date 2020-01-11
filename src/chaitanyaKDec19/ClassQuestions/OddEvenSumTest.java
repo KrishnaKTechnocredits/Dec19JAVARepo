f31c5a8deb91307ff4980efbdff89ddbd26dc67f
ChaitanyaKDec19 <chaitanya0912@gmail.com>
2020-01-11 11:36:12 +0530

    Class Questions completed

---
+++ src/chaitanyaKDec19/ClassQuestions/OddEvenSumTest.java
@@ -0,0 +1,33 @@
+package chaitanyaKDec19.ClassQuestions;
+
+public class OddEvenSumTest {
+
+	public static void main(String[] args) {
+
+		if (args.length == 0) {
+			System.out.println("Please enter valid arguments i.e. numbers");
+		} else {
+			displayOddEvenSum(args, args.length);
+		}
+
+	}
+
+	static void displayOddEvenSum(String[] args, int argsLength) {
+
+		int oddSum = 0;
+		int evenSum = 0;
+
+		for (int i = 0; i < argsLength; i++) {
+
+			if (Integer.parseInt(args[i]) % 2 == 0)
+				evenSum = evenSum + Integer.parseInt(args[i]);
+			else
+				oddSum = oddSum + Integer.parseInt(args[i]);
+		}
+
+		System.out.println("Total of even numbers is: " + evenSum);
+		System.out.println("Total of odd numbers is: " + oddSum);
+
+	}
+
+}
