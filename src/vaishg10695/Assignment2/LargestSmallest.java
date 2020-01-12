733e2823a6008563fa317f5c44cdd3d35fb2776a
vaishg10695 <vaishnavi.g10695@gmail.com>
2020-01-13 00:30:18 +0530

    Adding Assignment2 remaing program and as suggested done code formating for all progam

--- src/vaishg10695/Assignment2/LargestSmallest.java
+++ src/vaishg10695/Assignment2/LargestSmallest.java
@@ -3,29 +3,26 @@ Hint: For example, if input integer array is {2, 6, 3, 9, 11} and given sum is 9
 
 package vaishg10695.Assignment2;
 
-public class LargestSmallest 
-{
-	void larSmall(int[] a)
-	{
-		int max=0;
-		int min=99999;
-		for (int i=0;i<a.length;i++)
-		{
-			if(a[i]>max)
-				max=a[i];
-			if(a[i]<min)
-				min=a[i];
+public class LargestSmallest {
+	void larSmall(int[] a) {
+		int max = 0;
+		int min = 99999;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] > max)
+				max = a[i];
+			if (a[i] < min)
+				min = a[i];
 		}
-		System.out.println("largest value in array is: "+ max);
-		System.out.println("smallest value in array is: "+ min);
+		System.out.println("largest value in array is: " + max);
+		System.out.println("smallest value in array is: " + min);
 
 	}
-	public static void main(String[] args) 
-	{
-		LargestSmallest ls=new LargestSmallest();
-		int[] ary= {15,6,3,11,9};
+
+	public static void main(String[] args) {
+		LargestSmallest ls = new LargestSmallest();
+		int[] ary = { 15, 6, 3, 11, 9 };
 		ls.larSmall(ary);
-		
+
 	}
 
 }
--- src/vaishg10695/Assignment2/Q12commonNumber.java
+++ src/vaishg10695/Assignment2/Q12commonNumber.java
@@ -5,27 +5,23 @@
 
 package vaishg10695.Assignment2;
 
-public class Q12commonNumber 
-{
-	void comNum(int[]a1,int[]a2)
-	{
-		if(a1.length!=a2.length)
+public class Q12commonNumber {
+	void comNum(int[] a1, int[] a2) {
+		if (a1.length != a2.length)
 			System.out.println("given two array are not equal");
-		else
-		{
-			for(int i=0;i<a1.length;i++)
-				if(a1[i]==a2[i])
-				{
-					System.out.println("common values is ->"+ a1[i]);
+		else {
+			for (int i = 0; i < a1.length; i++)
+				if (a1[i] == a2[i]) {
+					System.out.println("common values is ->" + a1[i]);
 					return;
 				}
 		}
 	}
-	public static void main(String[] args) 
-	{
-		Q12commonNumber cn=new Q12commonNumber();
-		int[] ary1 = {1, 3, 6, 9, 7};
-	    int[] ary2 = {2, 5, 6, 5, 8};
+
+	public static void main(String[] args) {
+		Q12commonNumber cn = new Q12commonNumber();
+		int[] ary1 = { 1, 3, 6, 9, 7 };
+		int[] ary2 = { 2, 5, 6, 5, 8 };
 		cn.comNum(ary1, ary2);
 	}
 
