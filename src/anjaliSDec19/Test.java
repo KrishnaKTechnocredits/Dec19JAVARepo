cf35be2f17800c35ee37f7ef03d66ede40245026
Anjali Shroff <anjalishroff_57@yahoo.co.in>
2020-01-12 16:32:54 +0530

    Added conflict file 2

--- src/anjaliSDec19/Test.java
+++ src/anjaliSDec19/Test.java
@@ -3,6 +3,7 @@ package anjaliSDec19;
 public class Test {
 
 	public static void main(String[] args) {
+		System.out.println("Hi, how are you?!!");
 		System.out.println("Welcome to TechnoCredits Family.");
 	}
 }
