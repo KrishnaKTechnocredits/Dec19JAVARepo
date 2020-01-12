febf55be69664a8cf2b7f347297eecb2c154a328
priyankaDec19 <priyanka18031995@gmail.com>
2020-01-12 16:17:23 +0530

    changes done

--- src/priyankaDec19/Test.java
+++ src/priyankaDec19/Test.java
@@ -3,6 +3,7 @@ package priyankaDec19;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to TechnoCredits Family.");
+		//System.out.println("Welcome to TechnoCredits Family.");
+		System.out.println("Hello");
 	}
 }
