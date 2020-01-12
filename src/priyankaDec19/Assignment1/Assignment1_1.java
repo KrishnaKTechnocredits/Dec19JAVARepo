b43a540ccbf69485461d8f4157cbe2536c546921
priyankakDec19 <priyanka18031995@gmail.com>
2020-01-12 15:11:19 +0530

    Assignment 1 added

---
+++ src/priyankaDec19/Assignment1/Assignment1_1.java
@@ -0,0 +1,17 @@
+class Assignment1_1
+{
+	void Sum(int x, int y)
+	{
+		int sum= x+y;
+		System.out.println("Total is " + sum);
+	}
+	 public static void main(String args[])
+	 {
+		 Assignment1_1 as1= new Assignment1_1();
+		 as1.Sum(30,40);
+	}
+	
+	
+	
+	
+}
---
+++ src/priyankaDec19/GitTest1.java
@@ -0,0 +1,15 @@
+package priyankaDec19;
+/*
+ * "Hi Hello TechnoCredits Good"
+ * o/p : Total Words= 4 
+ */
+public class GitTest1 {
+	public static void main(String[] args) {
+		String s= "Hi Hello TechnoCredits Good";
+		String[] values= s.split(" ");
+		int size= values.length;
+		System.out.println(size);
+	}
+	
+
+}
