52d1edf6f516a5fba5aee3f679cd34536ff026b9
Sonal <sonalphaltane@gmail.com>
2020-01-17 21:21:09 +0530

    Assignment3- solutions for 1,7,10,11,12,13,15 added

---
+++ src/sonalPDec19/assignment3/ArmstrongNo.java
@@ -0,0 +1,30 @@
+package assignment3;
+/*
+ * WAP to find number is Armstrong or not?
+ */
+import java.util.Scanner;
+
+public class ArmstrongNo {
+
+	void findArmstrongNo(String inputNum) {
+		int cubeSum = 0;
+		for (int i = 0; i < inputNum.length(); i++) {
+			int convertInInteger = Integer.parseInt(String.valueOf(inputNum.charAt(i)));
+			cubeSum = cubeSum + (convertInInteger * convertInInteger * convertInInteger);
+		}
+		if (Integer.parseInt(inputNum) == cubeSum)
+			System.out.println("Number is Armstrong number.");
+		else
+			System.out.println("Number is not Armstrong number.");
+
+	}
+
+	public static void main(String[] args) {
+		ArmstrongNo armstrongno = new ArmstrongNo();
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter No:");
+		String no = s.nextLine();
+		armstrongno.findArmstrongNo(no);
+	}
+
+}
