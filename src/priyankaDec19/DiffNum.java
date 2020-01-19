7cf09e27ff20ceb15fac278fd8ed00e3139077a7
priyankaDec19 <priyanka18031995@gmail.com>
2020-01-19 14:32:56 +0530

    Diff java

---
+++ src/priyankaDec19/DiffNum.java
@@ -0,0 +1,12 @@
+package priyankaDec19;
+
+public class DiffNum {
+           public static void main(String[] args) {
+			String s= "s1i2d3d4h5i6";
+			boolean IsDigit= Character.isDigit(97);
+			System.out.println(Character.isLetter(32));
+			System.out.println(character.isLowerCase('A'));
+			m1();
+			-
+		}
+}
