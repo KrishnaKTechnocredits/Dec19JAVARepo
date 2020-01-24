f5e515bb4b72e510eba9d3e9b378c82bd3099cfa
ShitalSDec19 <shitalseware@gmail.com>
2020-01-24 19:29:06 +0530

    adding 14th solution of 3rd assignment

--- src/shitalSDec19/assignment3/Solution14.java
+++ src/shitalSDec19/assignment3/Solution14.java
@@ -1,4 +1,4 @@
-/*  WAP to find unique number from given array store that numbers in new array. 
+/*  WAP to find unique number from given array store that numbers in new array.
 a. firstArray:- {1,2,3,4,2,5} 
 b. secondArray:- {5,6,7,8,9,7} 
 c. unique array = {1,2,3,4,5,6,7,8,9} 
@@ -7,35 +7,49 @@ i. order can be anything  */
 package shitalSDec19.assignment3;
 
 public class Solution14 {
-	int [] arr3 = {};
-	void findUniqueNo(int [] arr1, int [] arr2)
-	{
-		if (arr1.length == arr2.length)
-		{
-			for (int i = 0; i<arr1.length; i++)
-			{
-				for (int j=0; j<arr2.length; j++)
-				{
-					if (arr1[i] != arr2[j])
-						
-				arr3 = arr2;
-					System.out.println(arr3[i]);
+
+	void findUniqueNo(int[] arr1, int[] arr2) {
+		int[] Unique1 = new int[arr1.length + arr2.length];
+		int[] Unique2 = new int[arr1.length + arr2.length];
+		boolean isFound = false;
+		boolean flag = false;
+		int cnt = 0;
+		if (arr1.length == arr2.length) {
+			for (int i = 0; i < arr1.length; i++) {
+				isFound = false;
+				for (int j = 0; j < arr2.length; j++) {
+					if (arr1[i] == arr2[j])
+						isFound = true;
 				}
-				
+				if (!isFound) {
+					Unique1[cnt] = arr1[i];
+					flag = false;
+					for (int k = i + 1; k < arr1.length; k++) {
+						if (arr1[1] == arr1[k])
+							flag = true;
+					}
+					if (flag == true)
+						System.out.println(arr1[i]);
+				}
 			}
-			
 		}
-		
-		
+		for (int i = 0; i < arr2.length; i++) {
+			isFound = false;
+			for (int j = 0; j < arr2.length; j++) {
+				if (arr1[i] == arr2[j])
+					isFound = true;
+			}
+			if (!isFound) {
+				Unique2[cnt] = arr2[i];
+				System.out.println(arr2[i]);
+			}
+		}
 	}
 
 	public static void main(String[] args) {
-		int [] arr1 = {1,2,3,4,2,5};
-		int [] arr2 = {5,6,7,8,9,7};
+		int[] arr1 = { 1, 2, 3, 4, 2, 5 };
+		int[] arr2 = { 5, 6, 7, 8, 9, 7 };
 		Solution14 solution14 = new Solution14();
 		solution14.findUniqueNo(arr1, arr2);
-		
 	}
-
-
 }
