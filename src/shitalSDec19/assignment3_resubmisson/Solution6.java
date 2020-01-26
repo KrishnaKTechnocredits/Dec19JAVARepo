f7494a00ac0f756415ba79445e40eba4eef546ef
ShitalSDec19 <shitalseware@gmail.com>
2020-01-26 19:49:57 +0530

    re-adding all solutions of Assignment 3

---
+++ src/shitalSDec19/assignment3_resubmisson/Solution6.java
@@ -0,0 +1,32 @@
+/* WAP to find reverse max length string 
+	a. Input array:- {“all the best”, “technocredits”, “”, “maharashtra”} 
+	b. Output Max length string :- Technocredits 
+	c. Max length string reverse value :- stiderconhcet  */
+package shitalSDec19.assignment3_resubmisson;
+
+public class Solution6 {
+
+	void findMaxLengthString(String[] arr) {
+		String reverse = "";
+		int maxLength = 0;
+		String maxLengthName = "";
+		for (int i = 0; i < arr.length; i++) {
+			if (maxLength < arr[i].length()) {
+				maxLength = arr[i].length();
+				maxLengthName = arr[i];
+				for (int j = maxLengthName.length() - 1; j >= 0; j--) {
+					reverse = reverse + maxLengthName.charAt(j);
+				}
+			}
+		}
+		System.out.println("output max length string is " + maxLengthName + " and its length is " + maxLength);
+		System.out.println("Max length string reverse value " + reverse);
+	}
+
+	public static void main(String[] args) {
+		String[] input = { "all the best", "technocredits", "", "maharashtra" };
+		Solution6 solution6 = new Solution6();
+		solution6.findMaxLengthString(input);
+
+	}
+}
---
+++ src/shitalSDec19/assignment3_resubmisson/Solution9.java
@@ -0,0 +1,26 @@
+/*  WAP to find average of user given array 
+ * a. Input :- {14, 32, 19, 14, 78,36}
+b. Output:- 32.16  */
+
+package shitalSDec19.assignment3_resubmisson;
+
+public class Solution9 {
+	double avg = 0;
+	int sum = 0;
+
+	double findAvg(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			sum = sum + arr[i];
+
+		}
+		avg = avg + (double) sum / arr.length;
+		return avg;
+	}
+
+	public static void main(String[] args) {
+		int[] nos = { 14, 32, 19, 14, 78, 36 };
+		Solution9 solution9 = new Solution9();
+		double avgNos = solution9.findAvg(nos);
+		System.out.println(avgNos);
+	}
+}
