c1fbca1cd615f0e2c59b951ed8995f6e5d13fd08
ankita <potdarankita58@gmail.com>
2020-01-28 22:03:05 +0530

    adding assign_3_sol-12

--- src/ankitaPDec19/Assignment3_1/Solution12.java
+++ src/ankitaPDec19/Assignment3_1/Solution12.java
@@ -8,18 +8,17 @@ package ankitaPDec19.Assignment3_1;
 
 public class Solution12 {
 	static String s = "";
-	public static void main(String [] args)
-	{
+
+	public static void main(String[] args) {
 		String name1 = "naman";
 		compare(name1);
-		if(name1.equals(s))
-		     System.out.println("The name is pallindrome " +s);
+		if (name1.equals(s))
+			System.out.println("The name is pallindrome " + s);
 	}
-	static String compare(String name1)
-	{
-		for (int i = name1.length()-1; i >= 0; i--)
-		{
-			 s = s + String.valueOf(name1.charAt(i));
+
+	static String compare(String name1) {
+		for (int i = name1.length() - 1; i >= 0; i--) {
+			s = s + String.valueOf(name1.charAt(i));
 		}
 		return s;
 	}
