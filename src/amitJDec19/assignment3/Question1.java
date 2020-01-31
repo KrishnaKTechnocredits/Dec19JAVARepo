3e33ee271731f09f232b1c12b40af9fc97793037
AmitJDec19 <amit.jay32@gmail.com>
2020-01-31 19:08:35 +0530

    Adding 3 Questions

---
+++ src/amitJDec19/assignment3/Question1.java
@@ -0,0 +1,24 @@
+package amitJDec19.assignment3;
+/*
+    *  
+   * *  
+  * * *  
+ * * * *  
+* * * * *  
+ */
+public class Question1 {
+	static void pattern(int num) {
+		for (int i=1;i<=num;i++) {
+			for (int k=1;k<=num-i;k++) {
+				System.out.print(" ");
+			}
+		for(int j=1;j<=i;j++) {
+			System.out.print("*"+" ");
+			}
+		System.out.println(" ");
+		}
+	}
+	public static void main(String[] args) {
+		pattern(5);
+	}
+}
