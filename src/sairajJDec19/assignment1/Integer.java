1fcc6f6895c18ca7b97e5b380c9d60f9ed83939d
SairajJDec19 <saairajj812@gmail.com>
2020-02-04 22:34:25 +0530

    Adding assignment 1

---
+++ src/sairajJDec19/assignment1/Integer.java
@@ -0,0 +1,12 @@
+package sairajJDec19.assignment1;
+class Integer
+{
+static void printInt(int start, int end){
+  for (int i= start;i<=end ;i++ ) {
+    System.out.println(i);
+  }
+}
+  public static void main(String[] args) {
+    printInt(5,10);
+}
+}
